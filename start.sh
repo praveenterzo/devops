6f41b476b531d6891ca4cf811ffaf62b477abfbc
praveenterzo <102901741+praveenterzo@users.noreply.github.com>
2025-06-26 16:58:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Installing Apache..."
-yum install -y httpd
-systemctl enable httpd
-systemctl start httpd
+yum install -y nginx
+systemctl enable nginx
+systemctl start nginx
 
 echo "Deployment complete."
